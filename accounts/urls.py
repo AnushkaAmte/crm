6e834ffd4a29b5b589bab39ae168b8baf3e0972f
Dracorexia <61610230+Dracorexia@users.noreply.github.com>
2021-04-16 16:42:14 +0530

    added decorator ,user access and admin access

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+    
     path('', views.home, name='home'),
+    path('user/',views.userPage,name="user-page"),
     path('products/', views.products, name='products'),
     path('customer/<str:pk_test>/', views.customer, name='customers'),
+    
     path('create_order/', views.create_order, name='create_order'),
     path('update_order/<str:pk>/', views.update_order, name='update_order'),
     path('delete_order/<str:pk>/', views.delete_order, name='delete_order'),
