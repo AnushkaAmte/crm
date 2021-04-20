d2e408fc2ac10866194d2d03ebe58f5ebffc5e23
AnushkaAmte <anushkaamte@gmail.com>
2021-04-20 21:38:51 +0530

    redundant changes

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,14 +5,16 @@ from django.contrib.auth.models import Group
 
 from .models import Customer
 
+
 def customer_profile(sender, instance, created, **kwargs):
-	if created:
-		group = Group.objects.get(name='customer')
-		instance.groups.add(group)
-		Customer.objects.create(
-			user=instance,
-			name=instance.username,
-			)
-		print('Profile created!')
+    if created:
+        group = Group.objects.get(name='customer')
+        instance.groups.add(group)
+        Customer.objects.create(
+            user=instance,
+            name=instance.username,
+        )
+        print('Profile created!')
+
 
 post_save.connect(customer_profile, sender=User)
